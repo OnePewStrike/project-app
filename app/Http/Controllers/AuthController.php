d95caa527c7c4a64f18a30dd36d26a94ff2a1c8c
Godwin <rualgodwin@gmail.com>
2022-09-23 07:39:53 +0800

    init

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1,1 +1,92 @@
-1
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Session;
+
+class AuthController extends Controller
+{
+    public function login()
+    {
+        return view("login");
+    }
+    public function registration()
+    {
+        return view("registration");
+    }
+
+    public function registerUser(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'username' => 'required|unique:users',
+            'password' => 'required',
+        ]);
+
+        $name = $request->name;
+        $username = $request->username;
+        $password = Hash::make($request->password);
+
+        $user = new User();
+        $user->name = $name;
+        $user->username = $username;
+        $user->password = $password;
+        $res = $user->save();
+
+        if ($res) {
+            return back()->with('success', 'Success: You have registered successfully');
+        } else {
+            return back()->with('fail', 'Error: An error occurred please try again');
+        }
+    }
+
+    public function loginUser(Request $request)
+    {
+        $request->validate([
+            'username' => 'required',
+            'password' => 'required',
+        ]);
+
+        $user = User::where('username', '=', $request->username)->first();
+        if ($user) {
+            if (Hash::check($request->password, $user->password)) {
+                $request->session()->put('loginId', $user->id);
+                return redirect('dashboard');
+            } else {
+                return back()->with('fail', 'This password is not matched.');
+            }
+        } else {
+            return back()->with('fail', 'This username is not registered.');
+        }
+    }
+
+    public function dashboard()
+    {
+        $data = array();
+        if (Session::has('loginId')) {
+            $data = User::where('id', '=', Session::get('loginId'))->first();
+        }
+        return view("dashboard", compact('data'));
+    }
+
+    public function home()
+    {
+        $data = array();
+        if (Session::has('loginId')) {
+            $data = User::where('id', '=', Session::get('loginId'))->first();
+        }
+        return view("home", compact('data'));
+    }
+
+
+    public function logout()
+    {
+        if (Session::has('loginId')) {
+            Session::pull('loginId');
+            return redirect('login');
+        }
+    }
+}
--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -1,1 +1,98 @@
-1
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Book;
+
+class BooksController extends Controller
+{
+    public function dashboard()
+    {
+        $data = Book::get();
+        return view('dashboard', compact('data'));
+    }
+
+    public function index()
+    {
+        $data = Book::get();
+        return view('maintenance', compact('data'));
+    }
+
+    public function addBook()
+    {
+        return view('add-book');
+    }
+
+    public function saveBook(Request $request)
+    {
+        $request->validate([
+            'booktitle' => 'required',
+            'publisher' => 'required',
+            'author' => 'required',
+            'yearpublished' => 'required',
+            'volume' => 'required',
+        ]);
+
+        $booktitle = $request->booktitle;
+        $publisher = $request->publisher;
+        $author = $request->author;
+        $yearpublished = $request->yearpublished;
+        $volume = $request->volume;
+
+        $book = new Book();
+        $book->BookTitle = $booktitle;
+        $book->Publisher = $publisher;
+        $book->Author = $author;
+        $book->YearPublished = $yearpublished;
+        $book->Volume = $volume;
+        $book->save();
+
+        return redirect()->back()->with('success', 'Success: Book Added Succesfully');
+    }
+
+    public function editBook($id)
+    {
+        $data = Book::where('id', '=', $id)->first();
+        return view('edit-book', compact('data'));
+    }
+
+    public function updateBook(Request $request)
+    {
+        $request->validate([
+            'booktitle' => 'required',
+            'publisher' => 'required',
+            'author' => 'required',
+            'yearpublished' => 'required',
+            'volume' => 'required',
+        ]);
+
+        $id = $request->id;
+        $booktitle = $request->booktitle;
+        $publisher = $request->publisher;
+        $author = $request->author;
+        $yearpublished = $request->yearpublished;
+        $volume = $request->volume;
+
+        Book::where('id', '=', $id)->update([
+            'booktitle' => $booktitle,
+            'publisher' => $publisher,
+            'author' => $author,
+            'yearpublished' => $yearpublished,
+            'volume' => $volume
+        ]);
+        return redirect()->back()->with('success', 'Success: Book Updated Succesfully');
+    }
+
+    public function deleteBook($id)
+    {
+        Book::where('id', '=', $id)->delete();
+        return redirect()->back()->with('success', 'Success: Book Deleted Succesfully');
+    }
+
+    public function viewBook($id)
+    {
+        $data = Book::where('id', '=', $id)->first();
+        return view('view-book', compact('data'));
+    }
+}
