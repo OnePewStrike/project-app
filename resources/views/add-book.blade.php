588f8a3e54333a95ead35799afece0cdefc1db26
Godwin <rualgodwin@gmail.com>
2022-09-23 07:36:48 +0800

    init

--- resources/views/add-book.blade.php
+++ resources/views/add-book.blade.php
@@ -1,1 +1,111 @@
-1
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Add Book</title>
+  <link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+    <!-- Header Section -->
+    <section class="header">
+      <a href="{{  url('dashboard') }}">
+        <h1 class="logo"> LIBRARY SYSTEM </h1>
+      </a>
+      <a href="#" class="user"> username </a>
+    </section>
+
+    {{-- Sub Header Section  --}}
+    <section class="sub-header">
+      <div class="left-content">
+        <ul class ='nav-links'>
+          <li><a href="{{ url('doashbard')}}">Dashboard</a></li>
+          <li><a href="{{ url('add-book')}}">Add Books</a></li>
+          <li><a href="#">Authors</a></li>
+          <li><a href="{{  url('maintenance') }}">Maintenance</a></li>
+        </ul>
+      </div>
+      <div class="right-content">
+        <a href="login" class="logout">Logout </a>
+        <img src="/images/logout-icon.png" alt="#">
+      </div>
+    </section>
+
+  {{-- Add Book Form  --}}
+  <section class="hero">
+    <div class="wrapper">
+      <div class="form signup">
+        <header>Add Book</header>
+        @if(Session::has('success'))
+        <div class="success-txt" role="alert">
+          {{ Session::get('success') }}
+        </div>
+        @endif
+        <form action="{{ url('save-book') }}" method="post">
+          @csrf
+          <div class="field input">
+            <label class="form-label">BookTitle</label>
+            @error('booktitle')
+            <div class="text-danger" role="alert">
+              {{ $message }}
+            </div>
+            @enderror
+            <input type="text" class='form-control' name='booktitle' placeholder="Enter Book Title" value="{{ old('booktitle') }}">
+          </div>
+          <div class="field input">
+            <label class="form-label">Publisher</label>
+            @error('publisher')
+            <div class="text-danger" role="alert">
+              {{ $message }}
+            </div>
+            @enderror
+            <input type="text" class='form-control' name='publisher' placeholder="Enter Publisher" value="{{ old('publisher') }}">
+          </div>
+          <div class="field input">
+            <label class="form-label">Author</label>
+            @error('author')
+            <div class="text-danger" role="alert">
+              {{ $message }}
+            </div>
+            @enderror
+            <input type="text" class='form-control' name='author' placeholder="Enter Author" value="{{ old('author') }}">
+          </div>
+          <div class="field input">
+            <label class="form-label">Year Published</label>
+            @error('yearpublished')
+            <div class="text-danger" role="alert">
+              {{ $message }}
+            </div>
+            @enderror
+            <input type="text" class='form-control' name='yearpublished' placeholder="Enter Year Published" value="{{ old('yearpublished') }}">
+          </div>
+          <div class="field input">
+            <label class="form-label">Volume</label>
+            @error('volume')
+            <div class="text-danger" role="alert">
+              {{ $message }}
+            </div>
+            @enderror
+            <input type="text" class='form-control' name='volume' placeholder="Enter Volume" value="{{ old('volume') }}">
+          </div>
+          <br>
+          <div class="actions">
+            <button type="submit" class="btn btn-primary">Submit</button>
+            <a href="{{ url('maintenance') }}" class="btn btn-secondary">Back</a>
+          </div>
+
+        </form>
+      </div>
+    </div>
+  </section>
+
+  {{-- Footer Section  --}}
+  <section class="footer">
+    <div class="footer-details">
+      <h5>@Library Management System</h5>
+      <h5>CS31 Architecture And Organization</h5>
+    </div>
+  </section>
+</body>
+</html>
--- resources/views/registration.blade.php
+++ resources/views/registration.blade.php
@@ -1,1 +1,82 @@
-1
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Registration</title>
+  <link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+  <!-- Header Section -->
+  <section class="header">
+    <a href="{{  url('#') }}">
+      <h1 class="logo"> LIBRARY SYSTEM </h1>
+    </a>
+  </section>
+
+  <section class="hero">
+    <div class="wrapper">
+      <div class="form signup" style="margin-top: 20px">
+        <header>Registration</header>
+        <form action="{{route('register-user')}}" method="post">
+          @if(Session::has('success'))
+          <div class="success-txt">
+            {{ Session::get('success') }}
+          </div>
+          @endif
+          @if(Session::has('fail'))
+          <div class="error-txt">
+            {{ Session::get('fail') }}
+          </div>
+          @endif
+          @csrf
+          <div class="field input">
+            <label for="name">Name</label>
+            @error('name')
+            <span class="text-danger"> 
+              {{ $message }} 
+            </span>
+            @enderror
+            <input type="text" class="form-control" placeholder="Enter your name" name="name" value="{{ old('name') }}">
+          </div>
+          <div class="field input">
+            <label for="username">Username</label>
+            @error('username')
+            <span class="text-danger"> 
+              {{ $message }} 
+            </span>
+            @enderror
+            <input type="text" class="form-control" placeholder="Enter your Username" name="username" value="{{ old('username') }}">
+          </div>
+          <div class="field input">
+            <label for="password">Password</label>
+            @error('password')
+            <span class="text-danger"> 
+              {{ $message }} 
+            </span>
+            @enderror
+            <input type="password" class="form-control" placeholder="Enter your password" name="password" value="{{ old('password') }}">
+          </div>
+          <div class="link">Already have an account? <a href="{{ url('login') }}"> Login now </a></div>
+          <div class="actions">
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </section>
+
+  {{-- Footer Section  --}}
+  <section class="footer">
+    <div class="footer-details">
+      <h5>@Library Management System</h5>
+      <h5>CS31 Architecture And Organization</h5>
+    </div>
+  </section>
+
+
+  <!-- JavaScript Bundle with Popper -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+</body>
+</html>
